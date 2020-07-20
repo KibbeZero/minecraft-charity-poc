12f0c9d462b39279401e863db66e943f86892cdb
Kibbe <justinkibbe@outlook.com>
2020-07-20 09:23:57 -0400

    We now require the mode to be loaded on client side Separated clientside and serverside execution of custom block.

--- src/main/java/com/kibbezero/extralife/blocks/WinklePoopBlock.java
+++ src/main/java/com/kibbezero/extralife/blocks/WinklePoopBlock.java
@@ -1,38 +1,36 @@
 package com.kibbezero.extralife.blocks;
 
-import java.util.Random;
-
-import net.minecraft.block.*;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.particles.ParticleTypes;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.potion.Effect;
-import net.minecraft.potion.Effects;
-import net.minecraft.client.renderer.*;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorldReader;
-import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
-import javax.swing.text.html.parser.Entity;
+import javax.annotation.Nonnull;
 
 public class WinklePoopBlock extends ModFlower {
     public WinklePoopBlock(Effect p_i49984_1_, int effectDuration, Properties properties) {
         super(p_i49984_1_, effectDuration, properties);
-        RenderTypeLookup.setRenderLayer(this, RenderType.getCutout());
+        if(FMLEnvironment.dist.isClient()) {
+            RenderTypeLookup.setRenderLayer(this, RenderType.getCutout());
+        }
     }
 
     @Override
-    protected boolean isValidGround(final BlockState state, final IBlockReader worldIn, final BlockPos pos) {
+    protected boolean isValidGround(final BlockState state, @Nonnull final IBlockReader worldIn, @Nonnull final BlockPos pos) {
         Block block = state.getBlock();
         return !block.equals(Blocks.AIR) && !block.equals(Blocks.CAVE_AIR) && !block.equals(Blocks.VOID_AIR) && !block.equals(this);
     }
 
+    @Nonnull
+    @SuppressWarnings("deprecation") //It says that overrides are fine, you just shouldn't use the base class (which means don't super)
     @Override
-    public BlockRenderType getRenderType(final BlockState state) {
+    public BlockRenderType getRenderType(@Nonnull final BlockState state) {
         RenderTypeLookup.setRenderLayer(this, RenderType.getCutoutMipped());
         return BlockRenderType.MODEL;
     }
